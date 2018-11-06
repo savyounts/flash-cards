53c70aa2a9e64c515a356fef4dc978adac517bde
Josh Thompson <thompsonjoshd@gmail.com>
2018-11-06 09:49:11 -0700

    tests pass, finish setting up test_helper

---
+++ test/test_helper.rb
@@ -0,0 +1,15 @@
+# you can "centralize" all your requirements into this test_helper file.
+# just require this file in any test, and it has access to all these
+# gems and "lib" files
+
+# here's the gems you want access to
+gem 'minitest', '~> 5.2'
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+
+# and here's all the lib files you need access to
+require './lib/flash_cards'
+require './lib/guess'
+require './lib/deck'
+require './lib/round'
